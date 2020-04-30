af01ea7bf3111d52ee8e76f68c57ecbfe74a06d5
Michelle Wingter <mwingter@gmail.com>
2020-04-30 15:05:38 -0300

    Desafio Bonus 3 complete

--- Desafio 1/ex.js
+++ Desafio 1/ex.js
@@ -10,6 +10,8 @@ let opponentType = "water";
 
 const turnText = document.getElementById('text');
 let isTurnHappening = false;
+let opponentParalised = false;
+let roundsParalised = 0;
 
 const playerAttacks = {
   thunderShock: {
@@ -79,7 +81,7 @@ function gameOver (winner) {
 
 // Check if attacks misses
 function willAttackMiss (accuracy) {
-  console.log(Math.floor(Math.random() * 100) > accuracy);
+  //console.log(Math.floor(Math.random() * 100) > accuracy);
   return Math.floor(Math.random() * 100) > accuracy;
 }
 
@@ -111,11 +113,25 @@ function updateOpponentHp(newHP) {
   opponentHpElement.style.width = barWidth + '%';
 }
 
+//DESAFIO BONUS 3:
+// Special property in 'Fatal Blow' that randonmly (50% chance) makes the opponent 
+// paralise, not being able to attack for 2 rounds. If the opponent is already paralised, nothing happens.
+function specialParalise(attack){
+  if(attack.name == "Fatal Blow" && !willAttackMiss(50) && !opponentParalised){
+    //console.log("Paralisado!!");
+    opponentParalised = true;
+  }
+  else{
+    //console.log("Nao paralisado")
+  }
+}
+
+
 // DESAFIO BONUS 2:
 // Weakness system that causes electric attacks to do double damage to water creatures
 function eletricWeakness(attack){
   if(opponentType == "water" && attack.type == "electric"){ 
-    console.log("FRACO") 
+    //console.log("FRACO") 
     return 2;  
   }
   else{
@@ -133,7 +149,7 @@ function playerAttack(attack) {
   //console.log(attack);
   // 0: return false if attack misses
   if(willAttackMiss(attack.accuracy) == true){
-    console.log("ERREI!");
+    //console.log("ERREI!");
     return false;
   }
   // 1: otherwise update opponents health and return true
@@ -141,6 +157,12 @@ function playerAttack(attack) {
     var novoHp = opponentHp;
     novoHp = novoHp - attack.power * eletricWeakness(attack);
     updateOpponentHp(novoHp);
+
+    specialParalise(attack); //it may or may not paralise the enemy for 2 rounds
+    if(opponentParalised){
+      roundsParalised++;
+    }
+
     return true;
   }
 }
@@ -195,18 +217,35 @@ function turn(playerChosenAttack) {
 
   // Wait 2000ms to execute opponent attack (Player attack animation time)
   setTimeout(() => {
-    // Randomly chooses opponents attack
-    const opponentChosenAttack = chooseOpponentAttack();
 
-    const didOpponentHit = opponentAttack(opponentChosenAttack);
+    if(!opponentParalised && roundsParalised < 3){ //If the opponent is not paralised
+      // Randomly chooses opponents attack
+      const opponentChosenAttack = chooseOpponentAttack();
 
-    // Update HTML text with the used attack
-    turnText.innerText = 'Opponent used ' + opponentChosenAttack.name;
+      const didOpponentHit = opponentAttack(opponentChosenAttack);
 
-    // Update HTML text in case the attack misses
-    if (!didOpponentHit) {
-      turnText.innerText += ', but missed!';
+      // Update HTML text with the used attack
+      turnText.innerText = 'Opponent used ' + opponentChosenAttack.name;
+
+      // Update HTML text in case the attack misses
+      if (!didOpponentHit) {
+        turnText.innerText += ', but missed!';
+      }
     }
+    else{
+      if(roundsParalised == 1){
+        turnText.innerText = 'Opponent Paralised for 2 rounds!';
+      }
+      else if(roundsParalised == 2){
+        turnText.innerText = 'Opponent Paralised for 1 more round!';
+        roundsParalised = 0;
+        opponentParalised = false;
+      }
+      if(roundsParalised == 3){
+        
+      }
+    }
+
 
     // Wait 2000ms to end the turn (Opponent attack animation time)
     setTimeout(() => {
